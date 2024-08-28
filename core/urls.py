3d9f0354029130b4bd5d91a0962818095416fb9b
suricatstar <caue.tamiarana@gmail.com>
2024-08-27 23:33:48 -0300

    somente fiz uma pagina index no projeto

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('paciente/', include('paciente.urls')),
     path('profissional/', include('profissional.urls')),
     
